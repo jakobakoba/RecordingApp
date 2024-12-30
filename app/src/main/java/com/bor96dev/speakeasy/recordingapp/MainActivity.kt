5d4a94159d997d4244e43f78aefab39b1c5dad7e
jakobakoba <borisgabyshev96dev@gmail.com>
2024-12-30 16:23:38 +0900

    permissions

--- app/src/main/java/com/bor96dev/speakeasy/recordingapp/MainActivity.kt
+++ app/src/main/java/com/bor96dev/speakeasy/recordingapp/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.bor96dev.speakeasy.recordingapp
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.core.content.ContextCompat
+import com.bor96dev.speakeasy.recordingapp.ui.theme.CoralRed
+import com.bor96dev.speakeasy.recordingapp.ui.theme.MintGreen
 import com.bor96dev.speakeasy.recordingapp.ui.theme.RecordingAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,8 +34,54 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RecordingAppTheme {
+                var isServiceRunning by remember { mutableStateOf(false) }
+                var hasNotificationPermission by remember {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        mutableStateOf(
+                            ContextCompat.checkSelfPermission(
+                                applicationContext,
+                                Manifest.permission.POST_NOTIFICATIONS
+                            ) == PackageManager.PERMISSION_GRANTED
+                        )
+                    } else mutableStateOf(true)
+                }
+                val permissionLauncher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.RequestPermission()
+                ) { isGranted ->
+                    hasNotificationPermission = isGranted
+                    if (hasNotificationPermission && !isServiceRunning) {
+                        // Launch media projection service
+                    }
+                }
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Button(
+                            onClick = {
+                                if (!hasNotificationPermission && Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                                    permissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                                } else {
+                                    if (isServiceRunning) {
+                                        // stop
+                                    } else {
+                                        //start
+                                    }
+                                }
+                            },
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = if (isServiceRunning) CoralRed else MintGreen
+                            )
+                        ) {
+                            Text(
+                                text = if (isServiceRunning) "Stop recording" else "Start recording",
+                                color = Color.Black
+                            )
+                        }
+                    }
                 }
             }
         }
